b01682244d8e7d4ece95ab4ede667f64c55d5027
Philipp Derksen <derksenphilipp@gmail.com>
2021-11-02 21:19:31 +0100

    First try.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  function onDrop(event) {
+    console.log("Dropped ", event.dataTransfer.getData("li"));
+
+  }
+
+  function onDrag(event) {
+    event.dataTransfer.setData("li", event.target)
+  }
+</script>
+
+<ul on:drop|preventDefault={onDrop}>
+  <li draggable="true" on:dragover|preventDefault on:dragstart={onDrag}>List Item 1</li>
+  <li draggable="true" on:dragover|preventDefault on:dragstart={onDrag}>List Item 2</li>
+  <li draggable="true" on:dragover|preventDefault on:dragstart={onDrag}>List Item 3</li>
+  <li draggable="true" on:dragover|preventDefault on:dragstart={onDrag}>List Item 4</li>
+  <li draggable="true" on:dragover|preventDefault on:dragstart={onDrag}>List Item 5</li>
+</ul>
+
+<style>
+  li {
+  cursor:move;
+  display:block;
+  padding:20px 10px;
+  background:white;
+  border-bottom:solid 1px gray;
+}
+</style>
